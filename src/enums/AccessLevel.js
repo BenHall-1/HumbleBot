5865bc7e4482260e13f721a0260782aa4264a749
Ben Hall <benmhall1998@gmail.com>
2020-08-11 22:21:08 +0100

    :sparkles: Added new client role to Access Level

--- src/enums/AccessLevel.js
+++ src/enums/AccessLevel.js
@@ -1,16 +1,22 @@
-const { SUPPORT_ROLE, BOT_ADMINS } = require('../utils/config');
+const { CLIENT_ROLE, SUPPORT_ROLE, BOT_ADMINS } = require('../utils/config');
 
 module.exports = {
   EVERYONE: 0,
-  SUPPORT: 1,
-  ADMIN: 2,
+  CLIENT: 1,
+  SUPPORT: 2,
+  ADMIN: 3,
   hasAccess: (member, accessLevel) => {
     switch (accessLevel) {
       case 0:
         return true;
       case 1:
-        return member.roles.cache.find((role) => role.id === SUPPORT_ROLE) !== null;
+        return member.roles.cache.find((role) => role.id === CLIENT_ROLE) !== null
+        || member.roles.cache.find((role) => role.id === SUPPORT_ROLE) !== null
+        || BOT_ADMINS.includes(member.id);
       case 2:
+        return member.roles.cache.find((role) => role.id === SUPPORT_ROLE) !== null
+        || BOT_ADMINS.includes(member.id);
+      case 3:
         return BOT_ADMINS.includes(member.id);
       default:
         return false;
