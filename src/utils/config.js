6c7f2928c51f6e62ba65381597ced1cfe09896c9
Ben Hall <benmhall1998@gmail.com>
2020-08-11 22:21:44 +0100

    :sparkles: Added new client role to AccessLevel and config

--- src/utils/config.js
+++ src/utils/config.js
@@ -3,6 +3,7 @@ const config = require('../../config/config.json');
 module.exports = {
   BOT_ADMINS: config.serverProperties.botAdmins,
   BOT_LOGO: config.botSettings.botLogo,
+  CLIENT_ROLE: config.serverProperties.clientRole,
   DISCORD_TOKEN: config.botSettings.token,
   PREFIX: config.botSettings.prefix,
   SUPPORT_ROLE: config.serverProperties.supportRole,
